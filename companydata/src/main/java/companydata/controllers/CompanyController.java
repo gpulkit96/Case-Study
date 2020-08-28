68faabf3da9983ceb15bff45eaee09a3941bc5ea
Pulkit Gupta <gupta.pulkit96@gmail.com>
2020-08-28 18:57:06 +0530

    stock and company updated

--- companydata/src/main/java/companydata/controllers/CompanyController.java
+++ companydata/src/main/java/companydata/controllers/CompanyController.java
@@ -1,14 +1,53 @@
 package companydata.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import companydata.entities.Company;
+import companydata.services.CompanyService;
 
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping("")
+    public List<Company> getAllCompanies(){
+        return companyService.getAllCompanies();
+    }
+    @GetMapping("{id}")
+    public Optional<Company> getCompanyById(@PathVariable Integer id){
+        return companyService.getCompany(id);
+    }
+
+    @PostMapping("")
+    public void AddCompany(@RequestBody Company company){
+    	companyService.addCompany(company);
+    }
+    @PutMapping("{id}")
+    public void UpdateCompany(@PathVariable Integer id, Company company){
+    	companyService.updateCompany(id, company);
+    }
+    @DeleteMapping("{id}")
+    public void DeleteCompany(@PathVariable Integer id){
+    	companyService.deleteCompany(id);
+    }
+}
+
+
+/*public class CompanyController {
 
 	@RequestMapping("/{name}")
 	public Company getCompany(@PathVariable("name") String name) {
@@ -18,3 +57,4 @@ public class CompanyController {
 		return company;
 	}
 }
+*/
--- companydata/src/main/java/companydata/services/CompanyService.java
+++ companydata/src/main/java/companydata/services/CompanyService.java
@@ -1,8 +1,37 @@
 package companydata.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import companydata.entities.Company;
+import companydata.repositories.CompanyRepository;
 
 @Service
 public class CompanyService {
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public List<Company> getAllCompanies() {
+        List<Company> companies = new ArrayList<>();
+        companyRepository.findAll()
+                .forEach(companies::add);
+        return companies;
+    }
+
+    public Optional<Company> getCompany(Integer id) {
+        return companyRepository.findById(id);
+    }
+    public void addCompany(Company company) {
+    	companyRepository.save(company);
+    }
+    public void updateCompany(Integer id, Company company) {
+    	companyRepository.save(company);
+    }
+    public void deleteCompany(Integer id) {
+    	companyRepository.deleteById(id);
+    }
 }
