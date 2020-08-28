f2d858b50621bec667f12a14c511bf31b5655084
Mohit <mohitmadan71@gmail.com>
2020-08-28 18:10:06 +0530

    adding sector microservice

---
+++ sector-service/src/main/java/com/stockmarket/sectorservice/controllers/SectorControllers.java
@@ -0,0 +1,39 @@
+package com.stockmarket.sectorservice.controllers;
+
+import com.stockmarket.sectorservice.entities.Sector;
+import com.stockmarket.sectorservice.services.SectorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/sectors")
+public class SectorControllers {
+
+    @Autowired
+    private SectorService sectorService;
+
+    @GetMapping("")
+    public List<Sector> getAllSectors(){
+        return sectorService.getAllSectors();
+    }
+    @GetMapping("{id}")
+    public Optional<Sector> getSectorById(@PathVariable Integer id){
+        return sectorService.getSector(id);
+    }
+
+    @PostMapping("")
+    public void AddSector(@RequestBody Sector sector){
+        sectorService.addSector(sector);
+    }
+    @PutMapping("{id}")
+    public void UpdateSector(@PathVariable Integer id, Sector sector){
+        sectorService.updateSector(id, sector);
+    }
+    @DeleteMapping("{id}")
+    public void DeleteSector(@PathVariable Integer id){
+        sectorService.deleteSector(id);
+    }
+}
---
+++ sector-service/src/main/java/com/stockmarket/sectorservice/services/SectorService.java
@@ -0,0 +1,37 @@
+package com.stockmarket.sectorservice.services;
+
+import com.stockmarket.sectorservice.entities.Sector;
+import com.stockmarket.sectorservice.repositories.SectorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SectorService {
+
+    @Autowired
+    private SectorRepository sectorRepository;
+
+    public List<Sector> getAllSectors() {
+        List<Sector> sectors = new ArrayList<>();
+        sectorRepository.findAll()
+                .forEach(sectors::add);
+        return sectors;
+    }
+
+    public Optional<Sector> getSector(Integer id) {
+        return sectorRepository.findById(id);
+    }
+    public void addSector(Sector sector) {
+        sectorRepository.save(sector);
+    }
+    public void updateSector(Integer id, Sector sector) {
+        sectorRepository.save(sector);
+    }
+    public void deleteSector(Integer id) {
+        sectorRepository.deleteById(id);
+    }
+}
