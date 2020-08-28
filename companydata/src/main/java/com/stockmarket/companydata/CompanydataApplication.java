d98dc3d63d8ba18b72c73b8080abdaad03d372eb
Pulkit Gupta <gupta.pulkit96@gmail.com>
2020-08-28 13:30:30 +0530

    company data

---
+++ companydata/src/main/java/com/stockmarket/companydata/CompanydataApplication.java
@@ -0,0 +1,13 @@
+package com.stockmarket.companydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanydataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanydataApplication.class, args);
+	}
+
+}
