f1f34f5630207262191fefd8e10cf7d5d43bcd44
Pulkit Gupta <gupta.pulkit96@gmail.com>
2020-08-29 13:10:59 +0530

    updated eureka server

--- companydata/src/main/java/companydata/CompanydataApplication.java
+++ companydata/src/main/java/companydata/CompanydataApplication.java
@@ -2,8 +2,10 @@ package companydata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompanydataApplication {
 
 	public static void main(String[] args) {
