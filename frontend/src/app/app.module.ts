216445da05ad91f69cf4707e822ab2293c27aed2
Mohit <mohitmadan71@gmail.com>
2020-09-01 16:58:52 +0530

    added company, excel and comparison components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewCompanyComponent } from './new-company/new-company.component';
+import { UploadExcelComponent } from './upload-excel/upload-excel.component';
+import { CompareChartsComponent } from './compare-charts/compare-charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCompanyComponent,
+    UploadExcelComponent,
+    CompareChartsComponent
   ],
   imports: [
     BrowserModule,
