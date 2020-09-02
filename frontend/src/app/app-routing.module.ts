2d715340f3c716fca47acaa05b2a6cfbe4208158
Pulkit Gupta <gupta.pulkit96@gmail.com>
2020-09-02 15:06:07 +0530

    routed components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompareChartsComponent } from './components/compare-charts/compare-charts.component';
+import { NewCompanyComponent } from './components/new-company/new-company.component';
+import { UploadExcelComponent } from './components/upload-excel/upload-excel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: CompareChartsComponent},
+  {path: 'new-company', component: NewCompanyComponent},
+  {path: 'upload-excel', component: UploadExcelComponent},
+
+  {path: "**", pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
