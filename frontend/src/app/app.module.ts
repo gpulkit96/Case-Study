0b6b03f2a3660ece55318cf355780c48b4d1b930
Mohit <mohitmadan71@gmail.com>
2020-09-02 09:54:35 +0530

    organizing components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewCompanyComponent } from './new-company/new-company.component';
-import { UploadExcelComponent } from './upload-excel/upload-excel.component';
-import { CompareChartsComponent } from './compare-charts/compare-charts.component';
+import { NewCompanyComponent } from './components/new-company/new-company.component';
+import { UploadExcelComponent } from './components/upload-excel/upload-excel.component';
+import { CompareChartsComponent } from './components/compare-charts/compare-charts.component';
 
 @NgModule({
   declarations: [
