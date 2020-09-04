f93c73ba2296542867f557840d67672892450b70
Pulkit Gupta <gupta.pulkit96@gmail.com>
2020-09-04 11:56:51 +0530

    line and bar-charts

---
+++ beta_trial/angular-charts-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+
+const routes: Routes = [
+  {path: 'line-chart', component: LineChartComponent},
+  {path: 'bar-chart', component: BarChartComponent},
+
+  {path: "**", pathMatch: 'full', redirectTo: 'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
